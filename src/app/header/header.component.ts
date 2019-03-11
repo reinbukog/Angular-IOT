a2d739a47106a40d128dd9891d35a717a2486b42
rein.labares <reynante.labares@truste.com>
2019-03-11 22:50:07 +0800

    added status for hardware

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
 import { FirebaseService } from '../services/firebase/firebase.service';
+import { Observable } from 'rxjs';
+import { IntervalObservable } from 'rxjs/observable/IntervalObservable';
+import { TimerObservable } from 'rxjs/observable/TimerObservable';
+
+import 'rxjs/add/operator/takeWhile';
 
 @Component({
   selector: 'app-header',
@@ -10,6 +15,13 @@ import { FirebaseService } from '../services/firebase/firebase.service';
 export class HeaderComponent implements OnInit {
 
   showHeader: boolean = true;
+  status: string = 'STAT_OFF';
+  previousStatus = this.status;
+  currentStatus = this.status;
+  alive: boolean = true;
+
+  previousTime = new Date();
+
   constructor(private router: Router,
               private activatedRoute: ActivatedRoute,
               private firebaseService: FirebaseService
@@ -26,6 +38,40 @@ export class HeaderComponent implements OnInit {
         }
       }
     });
+
+    const sensorStatus = this.firebaseService.getData('sensor/status');
+    sensorStatus.subscribe(stat => {
+      this.currentStatus = stat;
+    });
+
+    IntervalObservable.create(10000)
+      .takeWhile(()=> this.alive)
+      .subscribe(() => {
+
+        if (this.currentStatus == 'STAT_BOOT') {
+          this.status = 'Booting';
+          console.log('boot');
+        }
+        else if (this.currentStatus == 'STAT_OFF') {
+          this.status = 'HW OFF';
+          console.log('off');
+        }
+
+        else if (this.previousStatus != this.currentStatus) {
+          this.status = 'Ready';
+          console.log('ready');
+        }
+        else {
+          this.status = 'HW OFF';
+          console.log('off');
+          const statObject = {
+            status: 'STAT_OFF'
+          };
+          this.firebaseService.saveData('sensor', statObject);
+        }
+
+        this.previousStatus = this.currentStatus;
+      });
   }
 
   onLogout() {
